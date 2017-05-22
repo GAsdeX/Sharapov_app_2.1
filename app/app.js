52a3584b10f2f403985b7843cee07f0d21e72c8e
GAsdeX <grinya2102@gmail.com>
2017-05-22 20:14:41 +0300

    wtf

--- app/app.js
+++ app/app.js
@@ -1,26 +1,40 @@
 //requires
 
-(function(){
-  var SharapovApp = angular.module('SharapovApp', ['ngRoute']);
-  console.log('control lol');
+(function() {
+    var SharapovApp = angular.module('SharapovApp', ['ngRoute']);
+    console.log('control lol');
 
-  SharapovApp.config('$routeProvider', function($routeProvider){
-    $routeProvider
-      .when('/',{
-        templateUrl: "public/views/main.html",
-        controller: "mainController"
-      })
-      .when('/operations', {
-        templateUrl: 'public/views/operations.html',
-        controller: "operationsController"
-      })
-      .otherwise({ redirectTo: '/'});
-  })
-  SharapovApp.controller('mainController', function($scope){
+    SharapovApp.config(function($routeProvider) {
+        $routeProvider
+          .when('/', {
+            templateUrl: "public/views/main.html",
+            controller: "mainController"
+        }).when('/operations', {
+            templateUrl: 'public/views/operations.html',
+            controller: "operationsController"
+        }).otherwise({
+            redirectTo: '/'
+        });
+    });
+    SharapovApp.controller('mainController', function($scope, $http) {
+      $http({
+        method : "GET",
+        url : "/categories"
+        }).then(function mySucces(response) {
+            $scope.myWelcome = response.data;
+        }, function myError(response) {
+            $scope.myWelcome = response.statusText;
+      });
 
-  })
+      // $http.get('localhost:3001/categories/').then(function(response){
+      //   // $scope.categories  = response.data;
+      //   // console.log($scope.att);
+      // });
+    });
 
-  SharapovApp.controller('operationsController', function($scope){})
+    SharapovApp.controller('operationsController', function($scope, $http) {
+
+    });
 
 })();
 
--- server.js
+++ server.js
@@ -1,97 +1,86 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient;
-var ObjectID = require('mongodb').ObjectID;
-var db = require('./db');
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const OdjectID = require('mongodb').ObjectID;
+const app = express();
+app.use(bodyParser.urlencoded({
+    express: true
+}));
 
-var app = express();
+var db;
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html')
+    console.log(__dirname);
+    var cursor = db.collection('quotes').find().toArray(function(err, result) {
+            console.log(result);
+        })
+        // console.log(cursor);
+});
 
-// parse application/json
-app.use(bodyParser.json());
+
+app.get('/categories', function(req, res) {
+    db.collection('categories').find().toArray(function(err, result) {
+        if (err) console.log(err);
+        else {
+            res.send(result)
+        }
+    });
+});
+
+
+app.get('/operations', function(req, res){
+  db.collection('operations').find().toArray(function(err, result){
+    if (err) console.log(err);
+    else res.send(result);
+  });
+});
+
+
+app.get('/operations/:parent', function(req, res) {
+    console.log(req.params);
+    db.collection('operations').find({
+        parent: req.params.parent
+    }).toArray(function(err, result) {
+        if (err) console.log(err);
+        else {
+            res.send(result)
+        }
+    });
+});
+
+
+// app.put('operations/:id', function(req, res) {
+//     console.log();
+//     db.colection('operations').updateOne({
+//         _id: ObjectID(req.params.id)
+//     }, function(err, result) {
+//         if (err) console.log(err);
+//         else res.sendStatus(200);
+//     });
+// });
+
+
+app.get('/operations/:parent/:id', function(req, res) {
+    db.collection('operations').findOne(function(err, result) {
+        if (err) console.log(err);
+        else {
+            res.send(result)
+        }
+    });
+});
 
 
 
-app.get('/', function(req,res){
-  res.send('hello, lol');
-})
-
-app.get('/artists', function(req,res){
-  // res.send(artists);
-  db.get().collection('artists').find().toArray(function(err, docs){
-    if(err){
-      console.log(err);
-      return res.sendStatus(500);
-    }
-    res.send(docs);
-  })
-})
-
-app.get('/artists/:id', function(req, res){
-  // res.send('trest');
-  db.get().collection('artists').findOne({ _id: ObjectID(req.params.id)}, function(err, doc ){
-    if(err){
-      console.log(err);
-      return res.sendStatus(500);
-    }
-    res.send(doc);
-  })
-
-})
-
-app.post('/artists', function(req, res){
-  var artist = {
-    name : req.body.name
-  };
-  db.get().collection('artists').insert(artist, function(err, result){
-    if(err){
-      console.log(err);
-      res.sendStatus(500);
-    }
-    res.send(artist);
-  })
-})
-
-app.put('/artists/:id', function(req, res){
-  console.log('lol');
-  db.get().collection('artists').updateOne(
-    { _id: ObjectID(req.params.id) },
-    { name: req.body.name },
-    // console.log(req.body.name);
-    function(err, result){
-      if (err){
+MongoClient.connect('mongodb://localhost:27017/test', function(err, database) {
+    if (err) {
         console.log(err);
         return res.sendStatus(500)
-      }
-      res.sendStatus(200);
+    } else {
+        db = database;
+        app.listen(3000, function() {
+            console.log('done');
+
+        })
     }
-  )
-})
-
-app.delete('/artists/:id', function(req, res){
-  db.get().collection('artists').deleteOne(
-    { _id: ObjectID(req.params.id)},
-
-    function(err,result){
-      if(err){
-        return res,sendStatus(500);
-      }
-      res.sendStatus(200);
-    }
-  )
-})
-
-
-
-db.connect('mongodb://localhost:27017/myapi', function(err){
-  if (err){
-    return console.log(err);
-
-  }
-  app.listen(3015, function (){
-    console.log("API lol");
-  })
-
 })
